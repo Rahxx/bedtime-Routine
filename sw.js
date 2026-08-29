7771ab03f365e07f11c2af35193cac7656d7c295
Rahxx <82295312+Rahxx@users.noreply.github.com>
2026-08-30 08:17:04 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "bedtime-routine-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
