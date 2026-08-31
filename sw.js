756c141f5c706b5738da347bc85327f94e6d411c
Rahxx <82295312+Rahxx@users.noreply.github.com>
2026-08-31 19:26:07 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bedtime-routine-v23";
+const CACHE = "bedtime-routine-v24";
 const STATIC_ASSETS = ["./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
